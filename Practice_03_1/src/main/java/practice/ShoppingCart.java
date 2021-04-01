9c1792d666cf4f50c3f42220c2d0baa1a547a1c3
eogieglo <41413402+eogieglo@users.noreply.github.com>
2021-03-31 21:59:26 -0300

    Update ShoppingCart.java

--- Practice_03_1/src/main/java/practice/ShoppingCart.java
+++ Practice_03_1/src/main/java/practice/ShoppingCart.java
@@ -17,9 +17,23 @@ public class ShoppingCart {
         //Entry point of the program
         //code goes here in the code block
         //Write here
-        
+        System.out.println(" ");
         System.out.println("Welcome to the Shopping Cart");
         
         //System.out.println(<some string value>);  String literal 
+        
+        //Practice 4-1
+        String custName = "Mary Smith";
+        String itemDesc = "Shirt";
+        String message;
+        
+        //Assign de message variable
+        System.out.println(" ");
+        message = custName + " wants to a purchase a " + itemDesc;
+        
+        //Print and Run de code
+        System.out.println(" ");
+        System.out.println(message);
+        System.out.println(" ");
     }
 }
