7d3aa513fc5b009543c616955f0a7f8ba930ef8a
eogieglo <41413402+eogieglo@users.noreply.github.com>
2021-04-07 13:10:12 -0300

    Create Item.java

---
+++ Practice_Java_SE_8_Associate/src/main/java/practice_6_1/Item.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practice_6_1;
+
+/**
+ *
+ * @author Administrador
+ */
+public class Item {
+    public int itemID;
+    public String desc;
+    public double price;
+    public int quantity;
+}
