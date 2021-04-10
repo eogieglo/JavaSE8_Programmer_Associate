d5df9d9476f4b14536245b9473f52540653aab4e
eogieglo <41413402+eogieglo@users.noreply.github.com>
2021-04-10 10:19:59 -0300

    Create Anidados.java

---
+++ HerbertSchild_7ma_practica/src/main/java/com/mycompany/Capitulo_5/Anidados.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.Capitulo_5;
+
+/**
+ *
+ * @author Administrador
+ */
+        
+// Ejemplo de ciclos anidados.
+        
+public class Anidados {
+ public static void main (String args[]) {
+    
+     int i, j;
+    
+        for(i=0; i<10; i++) {
+            for(j=i; j<10; j++) {
+            System.out.print (".");
+            System.out.println ();
+            }
+        }
+ }
+}
